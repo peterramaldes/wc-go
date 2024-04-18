62bf01429823ef486bbd352a72db59146ec22a6a
Peter Admilson Ramaldes <peter.ramaldes@gmail.com>
2024-04-18 07:31:03 -0300

    Printing the bytes of the file

--- cmd/wcg/main.go
+++ cmd/wcg/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+const WIDTH = 10
 
 func main() {
-	fmt.Println("works!")
+	// Parsing the flags
+	// c := flag.Bool("c", false, "print the byte counts")
+	// flag.Parse()
+
+	// Get only the files from the args
+	var files []string
+	for _, v := range os.Args {
+		if checkIfFileExists(v) {
+			files = append(files, v)
+		}
+	}
+
+	var total int64
+	for _, v := range files {
+		bytes := fileBytes(v)
+		total += bytes
+		fmt.Printf("%*d %s\n", WIDTH, bytes, v)
+	}
+	fmt.Printf("%*d total\n", WIDTH, total)
+}
+
+func checkIfFileExists(path string) bool {
+	if strings.Contains(path, "wcg") {
+		return false
+	}
+	_, err := os.Stat(path)
+	return !errors.Is(err, os.ErrNotExist)
+}
+
+func fileBytes(path string) int64 {
+	f, err := os.Stat(path)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+
+	return f.Size()
 }
